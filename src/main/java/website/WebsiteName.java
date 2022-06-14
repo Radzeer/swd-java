fef34a11096d235fc7752bf238ef8a84cdc01569
hola <hollo.attila.82@gmail.com>
2022-06-14 11:56:51 +0200

    init

---
+++ src/main/java/website/WebsiteName.java
@@ -0,0 +1,23 @@
+package website;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebsiteName {
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://python.org");
+
+        driver.findElement(By.id("id-search-field")).click();
+        driver.findElement(By.id("id-search-field")).sendKeys("testing");
+        driver.findElement(By.id("submit")).click();
+
+        String result=driver.findElement(By.cssSelector("h3:nth-child(2)")).getText();
+        System.out.println(result);
+
+    }
+}
